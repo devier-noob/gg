4245a8829c3adde17921132d4918275c1c7da06e
Devier <denverobogne015@gmail.com>
2026-06-16 21:32:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Monitoring System</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">    
 </head>
 
 <body>
@@ -13,88 +13,112 @@
     <span id="imageStatus">Editing</span>
 </header>
 
+
 <div class="main">
+    
 
     <div class="viewer-panel">
 
+
         <div id="myDIV" class="toolbar">
 
-            <input type="file" id="imageUpload">
-
-            <button id="zoomIn">Zoom +</button>
-            <button id="zoomOut">Zoom -</button>
-
-            <button id="saveImageSetup" class="btn">Lock</button>
-            <button id="unlockImage" class="btn active" >Unlock</button>
-
-            <button id="normalModeBtn" class="btn" >Add Pin</button>
-            
-            
-            <button id="clearPin">Delete All Pins</button>
-
-            <!-- ADDED: MULTI SELECT MODE -->
-            <button id="selectModeBtn" class="btn">Select Pins</button>
-            <button id="createReportBtn">Create Report</button>
-
-
+                    <input type="file" id="imageUpload">
+                    <button id="saveImageSetup" class="btn">Lock</button>
+                    <button id="unlockImage" class="btn active" >Unlock</button>
+                    <button id="normalModeBtn" class="btn" >Add Pin</button>            
+                    <button id="clearPin">Delete All Pins</button>
+                    <button id="selectModeBtn" class="btn">Select Pins</button>
+                    <button id="createReportBtn">View All Pins</button> 
         </div>
-
+        <div class="zoom">
+            <button id="zoomIn" style="background-color: lightgray; color: black;" >🔍︎+</button>
+            <button id="zoomOut" style="background-color: lightgray; color: black;">🔍︎-</button>
+        </div>
+                
         <div id="viewer">
-
-            <img id="mapImage" draggable="false">
-            <div id="pinsLayer"></div>
-            <div id="selectBox"></div>
+                
+                    
+                <img id="mapImage" draggable="false">
+                <div id="pinsLayer"></div>
+                 <div id="selectBox"></div>
 
         </div>
+        
+        
+
+        
 
     </div>
+    <div class="forms">
+        <div id="pinForm" class="hidden" >
 
-    <!-- REPORT LIST -->
-    <div id="reportPanel">
-        <h3 style="position: fixed;background: white;width: 250px;padding: 10px 5px; top: 65px; display: flex;justify-content: space-between;">Reports<button id="clearHistory">Clear All</button></h3>
-        
-        <div id="reportList" style="margin-top:50px;"></div>
+                <h3>Pin Details</h3>
+
+                <input id="pinName" placeholder="Name">
+
+                <select id="pinStatus"  >
+                    <option value="">Status</option>
+                    <option>Normal</option> 
+                    <option>Warning</option>
+                    <option>Critical</option>
+                    
+                </select>
+                <button id="eventDrop" style="display: flex;justify-content: space-between; border: 1px solid lightgray; background: white;color: black; " ><p>Events</p> <p>⮟</p></button>
+                <div id="pinEvent" class="hidden">
+                    
+                    <button type="button" class="eventBtn" value="Event1">Event1</button>
+                    <button type="button" class="eventBtn" value="Event2">Event2</button>
+                    <button type="button" class="eventBtn" value="Event3">Event3</button>
+                    <button type="button" class="eventBtn" value="Event4">Event4</button>
+                    <button type="button" class="eventBtn" value="Event5">Event5</button>
+                    <button type="button" class="eventBtn" value="Event6">Event6</button>
+                    <button type="button" class="eventBtn" value="Event7">Event7</button>
+                    <button type="button" class="eventBtn" value="Event8">Event8</button>
+                    
+
+                    
+
+
+
+                </div>
+                <h5>Event Logs</h5>
+                <div id="pinEventLogs">
+                    
+                    <div id="eventLogs"></div>
+                </div>
+
+                <textarea id="pinDescription" placeholder="Description"></textarea>
+                <div style="display: flex;flex-direction: row;justify-content: center;gap: 10px;">
+                    <button id="savePinBtn">Save</button>
+                    <button id="deletePinBtn">Delete</button>
+                </div> 
+
+        </div>
         
     </div>
+    
 
 
 </div>
 
-<!-- PIN FORM -->
-<div id="pinForm" class="hidden">
-
-    <h3>Pin Details</h3>
-
-    <input id="pinName" placeholder="Name">
-
-    <select id="pinStatus">
-        <option value="" place >Status</option>
-        <option>Normal</option> 
-        <option>Warning</option>
-        <option>Critical</option>
-        
-    </select>
-
-    <textarea id="pinDescription" placeholder="Description"></textarea>
-
-    <button id="savePinBtn">Save</button>
-    <button id="deletePinBtn">Delete</button>
-
+<div id="reportPanel">
+                <h3 style="display: flex; justify-content: space-between;">Activity Logs<button id="clearHistory" style="font-size: 20px; padding: 5px 10px; " >🗑</button></h3>
+                
+                <div id="reportList"></div>
+                
 </div>
 
 <!-- REPORT FORM -->
 <div id="reportForm" class="hidden">
 
-    <h3>Create Report</h3>
-
-    <input id="reportTitle" placeholder="Report Title">
-    <textarea id="reportDesc" placeholder="Description"></textarea>
-
-    <button id="saveReportBtn" type="button">Save Report</button>
-    <button id="cancelReportBtn" type="button">Cancel</button>
+    <div style="display: flex; justify-content: space-between;position:fixed;padding: 15px 10px;border-top-left-radius:10px;border-top-right-radius:10px; margin-top: -68px;margin-left: -14px; width:260px; background: white;">
+        <h3>Pin Manager</h3>
+        <button id="cancelReportBtn" type="button" style="padding: 5px; margin-top: -3px; margin-left:80px;background: white;color: #1f2937;size: 10px;" >✖</button>
+    </div>
+    
+    <div id="pinContainerList"></div>
 
     
-    <div id="selectedPreview"></div>
 
 </div>
 <script>
